82b4b246cd9047c530bfa3bb88cf317ed9044846
Amy Brown <32877781+amy-hashi@users.noreply.github.com>
2018-08-07 10:25:46 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-#resource "aws_instance" "amy-testing" {
-#  ami           = "ami-1853ac65"
-#  instance_type = "t2.micro"
-#  tags {
-#    "Name" = "amy test"
-#    "Tag1" = "tag 1"
-#  }
-#}
+resource "aws_instance" "amy-testing" {
+  ami           = "ami-1853ac65"
+  instance_type = "t2.micro"
+  tags {
+    "Name" = "amy test"
+    "Tag1" = "tag 1"
+  }
+}

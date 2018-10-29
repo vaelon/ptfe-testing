ab714209b1f8f1aafa11749c31ffac20bd516b0d
vaelon <rodriguez.jesse@gmail.com>
2018-10-29 18:16:15 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-#resource "aws_instance" "amy-testing" {
-#  ami           = "ami-1853ac65"
-#  instance_type = "t2.micro"
-#  tags {
-#    "Name" = "amy test"
-#    "Tag1" = "tag 1"
-#    "Tag2" = "tag 2"
-#  }
-#}
+resource "aws_instance" "jesser-testing" {
+ami           = "ami-0f65671a86f061fcd"
+instance_type = "t2.micro"
+tags {
+"Name" = "jesser-testing"
+"Tag1" = "tag 1"
+"Tag2" = "tag 2"
+}
+}

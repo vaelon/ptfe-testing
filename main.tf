550c2553d3f7f3806fcf8d1c587a48676a5fd7a0
vaelon <rodriguez.jesse@gmail.com>
2018-10-30 10:52:36 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "jesser-testing" {
-ami           = "ami-0f65671a86f061fcd"
+ami           = "ami-0ac019f4fcb7cb7e6"
 instance_type = "t2.micro"
 tags {
 "Name" = "jesser-testing"
